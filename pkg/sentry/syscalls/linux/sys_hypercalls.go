5bbe2b8ae3e0127ad5c2a6a432f7ed3d540c2759
Deepak Sirone <deepaksirone94@gmail.com>
2019-10-23 19:42:41 -0500

    Working hypercall printing vma info

--- pkg/sentry/syscalls/linux/sys_hypercalls.go
+++ pkg/sentry/syscalls/linux/sys_hypercalls.go
@@ -3,8 +3,32 @@ package linux
 import (
 	"gvisor.dev/gvisor/pkg/sentry/arch"
 	"gvisor.dev/gvisor/pkg/sentry/kernel"
+	"gvisor.dev/gvisor/pkg/sentry/usermem"
 )
 
 func Hypercall1(t *kernel.Task, args arch.SyscallArguments) (uintptr, *kernel.SyscallControl, error) {
+	ip := usermem.Addr(t.Arch().IP())
+	ar, err := ip.RoundDown().ToRange(usermem.PageSize)
+	t.Infof("Hypercall1: The current IP value is %x", ip)
+
+	if err == false {
+		t.Infof("Hypercall1 Failed to get Range")
+		return 0, nil, nil
+	}
+
+	t.Infof("Hypercall1 Just before GetVMAsLocked")
+
+	vma, _, e := t.MemoryManager().GetVMA(t, ar, usermem.AnyAccess, true)
+	if e != nil {
+		t.Infof("Hypercall1 Failed to get VMA")
+		return 0, nil, nil
+	}
+
+	t.Infof("Hypercall1 Just before GetName")
+	v := vma.ValuePtr().GetName(t)
+	i := vma.ValuePtr().GetInodeID()
+	d := vma.ValuePtr().GetDeviceID()
+
+	t.Infof("Hypercall1: Mapped name of VMA: %s, Inode Number: %x, Device Number: %x", v, i, d)
 	return 0, nil, nil
 }
