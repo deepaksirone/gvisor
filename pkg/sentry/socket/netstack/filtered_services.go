d9282b2ae3872c04c8ad34e772f1a80da12cdff3
Deepak Sirone <deepaksirone94@gmail.com>
2020-06-09 15:31:14 -0500

    Add blacklist

---
+++ pkg/sentry/socket/netstack/filtered_services.go
@@ -0,0 +1,20 @@
+package netstack
+
+var ServiceBlackList = map[string]struct{}{
+	"nginx-thrift":                 struct{}{},
+	"compose-post-redis":           struct{}{},
+	"home-timeline-redis":          struct{}{},
+	"media-memcached":              struct{}{},
+	"media-mongodb":                struct{}{},
+	"post-storage-memcached":       struct{}{},
+	"post-storage-mongodb":         struct{}{},
+	"social-graph-mongodb":         struct{}{},
+	"social-graph-redis":           struct{}{},
+	"url-shorten-memcached":        struct{}{},
+	"url-shorten-mongodb":          struct{}{},
+	"user-memcached":               struct{}{},
+	"user-mongodb":                 struct{}{},
+	"user-timeline-mongodb":        struct{}{},
+	"user-timeline-redis":          struct{}{},
+	"write-home-timeline-rabbitmq": struct{}{},
+}
